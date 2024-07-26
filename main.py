96f3a890f91e1905b54334bd7b0505066712f0b4
captainBashOrg <captain@sinn.ru>
2024-07-26 07:20:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,27 @@
 
 def test_function():
     def inner_function():
+        def aaa():
+            c = "123"
+            print(c)
+
+        #global a
+        a= 3
         print("Я в области видимости функции test_function")
         #print('__main__')
+        print (a)
+        aaa()
 
-    inner_function()
+    inner_function()   # вызов  inner_function()
+
+
 
 
 print ("Пространство имен\n\n")
 
+#inner_function()   # вызов  inner_function()
+# а вот здесь вызов inner_function() невозможен, она в пространстве имен test_function
+
 
 test_function()
+
